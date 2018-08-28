f9da4e7b366b721839e27d3ac508449b40a0268a
didier1969 <fccourchapoix@gmail.com>
2018-08-28 04:04:38 +0200

    Update state.js

--- src/store/app_store/state.js
+++ src/store/app_store/state.js
@@ -2,7 +2,7 @@ export default {
   odooUrl: 'https://zdconfort.ch',
   odooPort: '443',
   odooDb: 'proto-odoo11-zdc01',
-  odooUserName: 'eden@outlook.com',
+  odooUserName: 'eden@zdconfort.ch',
   odooPassword: 'upwork01',
   odoo: null,
   odooUser: null
